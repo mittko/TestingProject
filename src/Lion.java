ae267a3dc7c63d49f2e10bcad9a2898e8dfcf0ee
mitko_lazarov <d.alexandrov3@gmail.com>
2022-05-31 16:17:43 +0300

    AddedWeightProperty

--- src/Lion.java
+++ src/Lion.java
@@ -1,10 +1,12 @@
 public class Lion extends Cat {
     public String color = "yellow";
+    public float weight = 200f;
 
     @Override
     public String toString() {
         return super.toString() + " " + "Lion{" +
                 "color='" + color + '\'' +
+                ", weight=" + weight +
                 '}';
     }
 }

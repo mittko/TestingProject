71db476493f4818a539e34a5f38ea41186e81b38
Димитър Александров <d.alexandrov3@gmail.com>
2022-05-31 17:06:29 +0300

    AddedSex

--- src/Lion.java
+++ src/Lion.java
@@ -1,12 +1,15 @@
 public class Lion extends Cat {
     public String color = "yellow";
     public float weight = 200f;
-
+    public String sex = "female";
     @Override
     public String toString() {
-        return super.toString() + " " + "Lion{" +
+        return "Lion{" +
                 "color='" + color + '\'' +
                 ", weight=" + weight +
+                ", sex='" + sex + '\'' +
                 '}';
     }
+
+
 }
